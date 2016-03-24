9d6545e0a74839c17cfe0093e993e6ae95d31201
Rob DeCarlo <rdecarlo73@gmail.com>
2016-03-24 14:13:25 -0400

    xcode generated

---
+++ Test App/Test AppTests/Test_AppTests.swift
@@ -0,0 +1,36 @@
+//
+//  Test_AppTests.swift
+//  Test AppTests
+//
+//  Created by Rob DeCarlo on 3/24/16.
+//  Copyright © 2016 Rob DeCarlo. All rights reserved.
+//
+
+import XCTest
+@testable import Test_App
+
+class Test_AppTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
